26e51c2a42b9d8cee0214d9d8d96f122599146d0
chenchunyang <2323740041@qq.com>
2024-06-02 19:00:12 +0800

    feat: flask

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,2 +1,4 @@
 export const BackendUrl = 'http://localhost:3000';
-export const PBackendUrl = 'http://localhost:5000';
+// export const PBackendUrl = 'http://localhost:5000';
+// export const PBackendUrl = 'http://localhost:5000';
+export const PBackendUrl = 'http://flask-backend:5000'; // 使用服务名称
