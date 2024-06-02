f547298d52acc7679072252e3bb7a5713ebef737
chenchunyang <2323740041@qq.com>
2024-06-02 21:19:29 +0800

    feat: cros

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://122.51.139.146', // 允许的源，可以设置为前端的URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
