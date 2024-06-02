01a30c8b2a128b6c9937e48a76240011cc3c10a5
chenchunyang <2323740041@qq.com>
2024-06-02 20:52:40 +0800

    flask

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,4 +1,4 @@
 export const BackendUrl = 'http://localhost:3000';
 // export const PBackendUrl = 'http://localhost:5000';
 // export const PBackendUrl = 'http://localhost:5000';
-export const PBackendUrl = 'http://flask-backend:5000'; // 使用服务名称
+export const PBackendUrl = 'http://graduation-project-flask-backend-1:5000'; // 使用服务名称
